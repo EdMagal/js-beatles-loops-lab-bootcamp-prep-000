78f0f3caff2afaf1ae9a5597c96d4d3d30985474
Eduardo Magalhaes <ed83isme@gmail.com>
2018-02-27 17:44:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 var theBeatlesPlay = (musicians, instruments) => Array(musicians.length).fill(0).map((x,i)=>`${musicians[i]} plays ${instruments[i]}`);
+
 var johnLennonFacts = arr => arr.map(x=>x+"!!!");
+
 var iLoveTheBeatles = num => num<=15?Array(15-num).fill("I love the Beatles!"):["I love the Beatles!"];
